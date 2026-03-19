177eb17d6e1990c98cbf164bcdbaaa9d5d73662a
mesrwi-arch <mesrwikk@gmail.com>
2026-03-19 19:43:19 +0900

    강의자료 그대로 구현 1. User 모델 2. UserSerializer 3. authentication - exceptions.py, token.py 4. api - views.py, urls.py 5. config - urls.py

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,3 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    SOCIAL_TYPE = [
+        ('Naver', '네이버'),
+        ('Kakao', '카카오'),
+        ('Google', '구글'),
+    ]
+    JOB_CHOICE = [
+        ('AI Application Engineer', 'AI 서비스 개발자'),
+        ('MLOps Engineer', 'MLOps 엔지니어'),
+        ('Data Scientist', '데이터 사이언티스트'),
+    ]
+
+    name = models.CharField(max_length=10, null=True)
+    social = models.CharField(max_length=10, choices=SOCIAL_TYPE, null=True)
+    job = models.CharField(max_length=30, choices=JOB_CHOICE, null=True)
