76f215deba89f8aad63a0b475f91151b4879168b
mesrwi-arch <mesrwikk@gmail.com>
2026-03-19 20:02:06 +0900

    api/urls.py 수정 refresh, logout path 추가

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', Register.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('hello-world/', HelloWorldView.as_view(), name='hello-world'),
+    path('refresh/', RefreshView.as_view(), name='refresh'),
+    path('logout/', Logoutview.as_view(), name='logout')
 ]
