af877e55be2e17ca26082a1e2161f280480fd541
mesrwi <mjk9917@gmail.com>
2026-05-26 00:54:40 +0900

    backend 개발 초기화

--- backend/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import QuizHistoryView, JobTestHistoryView
-
-urlpatterns = [
-    path('mypage/quiz-history/', QuizHistoryView.as_view(), name='quiz_history'),
-    path('mypage/job-test-history/', JobTestHistoryView.as_view(), name='job-test_history')
-]
